a29b384258227609104ac2051d83c4e4ea13108b
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-04-29 22:22:57 +0900

    Refactor: Replace deprecated Icons API

--- Library/app/src/main/java/com/example/library/ui/screens/BookshelfDetailsScreen.kt
+++ Library/app/src/main/java/com/example/library/ui/screens/BookshelfDetailsScreen.kt
@@ -10,7 +10,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -48,7 +48,7 @@ fun LibraryDetailsScreen(
                 onClick = {detailsScreenParams.onBackPressed(data)}
             ) {
                 Icon(
-                    imageVector= Icons.Default.ArrowBack,
+                    imageVector= Icons.AutoMirrored.Default.ArrowBack,
                     contentDescription = stringResource(R.string.back),
                     modifier= Modifier
                         .fillMaxWidth()
