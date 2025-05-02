4f86168097b1cc06d322894506b6340de4831055
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-05-02 15:15:11 +0900

    Refactor: Change uiState from sealed interface to sealed class

--- Library/app/src/main/java/com/example/library/ui/LibraryUiState.kt
+++ Library/app/src/main/java/com/example/library/ui/LibraryUiState.kt
@@ -7,16 +7,16 @@ import com.example.library.network.BookInfo
 import com.example.library.network.Image
 import kotlinx.coroutines.flow.Flow
 
-sealed interface LibraryUiState{
+sealed class LibraryUiState{
     data class Success(
         val list :PageData,
         val bookmarkList:MutableList<Book> = mutableListOf(),
         val currentTabType:NavigationMenuType=NavigationMenuType.Books,
         val currentItem : MutableMap <NavigationMenuType,BookInfo> = mutableMapOf(),
         val isShowingHomepage: Boolean = true
-    ):LibraryUiState
-    object Error : LibraryUiState
-    object Loading: LibraryUiState
+    ): LibraryUiState()
+    object Error : LibraryUiState()
+    object Loading: LibraryUiState()
 }
 
 val defaultBookInfo
