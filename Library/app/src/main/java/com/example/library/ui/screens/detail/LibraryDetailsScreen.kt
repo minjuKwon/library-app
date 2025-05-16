d8429e5f0b65c89926215f21254579d7afef95ce
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-05-16 17:18:23 +0900

    Feat: Add ui components in detail screen

--- Library/app/src/main/java/com/example/library/ui/screens/detail/LibraryDetailsScreen.kt
+++ Library/app/src/main/java/com/example/library/ui/screens/detail/LibraryDetailsScreen.kt
@@ -1,31 +1,45 @@
 package com.example.library.ui.screens.detail
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowCircleUp
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material.icons.filled.ThumbUpOffAlt
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.library.R
@@ -49,7 +63,10 @@ fun LibraryDetailsScreen(
         if(isNotFullScreen){
             IconButton(
                 onClick = {detailsScreenParams.onBackPressed(data)},
-                modifier=Modifier.padding(dimensionResource(R.dimen.padding_lg))
+                modifier=Modifier.padding(
+                    horizontal=dimensionResource(R.dimen.padding_lg),
+                    vertical= dimensionResource(R.dimen.padding_xs)
+                )
             ) {
                 Icon(
                     imageVector= Icons.AutoMirrored.Default.ArrowBack,
@@ -61,7 +78,10 @@ fun LibraryDetailsScreen(
                 )
             }
         }
-        LazyColumn{
+        LazyColumn(
+            modifier=Modifier
+                .padding(bottom= dimensionResource(R.dimen.padding_sm))
+        ){
             item{
                 DetailsScreenContent(getCurrentItem(libraryUiState))
                 if (detailsScreenParams.isDataReadyForUi) {
@@ -97,17 +117,21 @@ private fun DetailsScreenContent(book: BookInfo){
                     )
                 }
             }
-            DetailScreenContentInformation(book)
+            DetailsScreenContentInformation(book)
         }
 
         book.description?.let {
-            DetailScreenContentDescription(it)
+            DetailsScreenContentDescription(it)
         }
+
+        DetailsScreenLibraryInformation()
+
+        DetailsScreenComment()
     }
 }
 
 @Composable
-private fun DetailScreenContentInformation(
+private fun DetailsScreenContentInformation(
     book: BookInfo
 ){
     OutlinedCard(
@@ -176,7 +200,7 @@ private fun DetailScreenContentInformation(
 }
 
 @Composable
-private fun DetailScreenContentDescription(
+private fun DetailsScreenContentDescription(
     text:String
 ){
     OutlinedCard(
@@ -198,6 +222,168 @@ private fun DetailScreenContentDescription(
                 modifier =Modifier
                     .padding(top= dimensionResource(R.dimen.padding_xs))
             )
+        }
+    }
+}
+
+@Composable
+private fun DetailsScreenLibraryInformation(){
+    Card(
+        modifier=Modifier
+            .fillMaxWidth()
+            .padding(top= dimensionResource(R.dimen.padding_md))
+    ){
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier=Modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal=dimensionResource(R.dimen.padding_xl),
+                    vertical= dimensionResource(R.dimen.padding_md)
+                )
+        ){
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text(text="청구기호")
+                Text(text="abcd.123")
+            }
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text(text="위치")
+                Text(text="3층 인문학실")
+            }
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text(text="대출상태")
+                Text(text="대출가능")
+            }
+        }
+    }
+}
+
+@Composable
+private fun DetailsScreenComment(){
+    Column{
+        CommentTextField()
+        CommentInformation()
+        for(i in 0..5){
+            CommentListItem()
+        }
+    }
+}
+
+@Composable
+private fun CommentTextField(){
+    OutlinedTextField(
+        value = "",
+        onValueChange = {},
+        trailingIcon={
+            Icon(
+                imageVector = Icons.Filled.ArrowCircleUp,
+                contentDescription = stringResource(R.string.comment_upload),
+                modifier= Modifier
+                    .clickable {  }
+                    .padding(
+                        dimensionResource(R.dimen.padding_md)
+                    )
+            )
+        },
+        keyboardOptions= KeyboardOptions.Default.copy(
+            imeAction= ImeAction.Done
+        ),
+        keyboardActions= KeyboardActions(
+            onDone = {},
+        ),
+        modifier= Modifier
+            .fillMaxWidth()
+            .padding(
+                top =dimensionResource(R.dimen.padding_lg)
+            )
+    )
+}
+
+@Composable
+private fun CommentInformation(){
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier=Modifier
+            .padding(
+                top= dimensionResource(R.dimen.padding_xl),
+                start= dimensionResource(R.dimen.padding_xs),
+                end= dimensionResource(R.dimen.padding_xs)
+            )
+            .fillMaxWidth()
+    ){
+        Text("123")
+        val options= listOf("공감순","최신순")
+        var selectedOption by remember{mutableStateOf("공감순")}
+        Row {
+            options.forEach { option->
+                Spacer(modifier=Modifier.width(dimensionResource(R.dimen.padding_lg)))
+                Text(
+                    text=option,
+                    color = if(selectedOption==option) Color.Black
+                    else Color.LightGray,
+                    modifier=Modifier
+                        .clickable { selectedOption= option }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun CommentListItem(){
+    OutlinedCard(
+        modifier=Modifier
+            .fillMaxWidth()
+            .padding(top= dimensionResource(R.dimen.padding_md))
+    ) {
+        Column(
+            modifier=Modifier
+                .padding(dimensionResource(R.dimen.padding_sm))
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier=Modifier.fillMaxWidth()
+            ){
+                Text(
+                    text="작성자",
+                    modifier=Modifier
+                        .padding(end=dimensionResource(R.dimen.padding_sm))
+                )
+                Text("2025-00-00")
+            }
+            Text(
+                text="댓글내용댓글내용댓글내용댓글내용댓글내용댓글내용댓글내용댓글내용댓글내용댓글내용댓글내용",
+                modifier=Modifier
+                    .padding(top=dimensionResource(R.dimen.padding_sm))
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier=Modifier.fillMaxWidth()
+            ){
+                Text(text="더보기")
+                Spacer(modifier=Modifier.weight(1f))
+                IconButton(
+                    onClick = {}
+                ) {
+                    Icon(
+                        imageVector = if(false){Icons.Default.ThumbUp}
+                        else {Icons.Default.ThumbUpOffAlt},
+                        contentDescription = stringResource(R.string.thumbUp_comment)
+                    )
+                }
+                Text("24")
+            }
         }
     }
 }
