bbd225c6166f0e9a76ca6ec1ac0f601e4d3eefaf
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-05-23 20:55:37 +0900

    Feat: Create LibraryUserUsageScreen

---
+++ Library/app/src/main/java/com/example/library/ui/screens/user/LibraryUserUsageScreen.kt
@@ -0,0 +1,198 @@
+package com.example.library.ui.screens.user
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import com.example.library.R
+import com.example.library.ui.BackIconButton
+import com.example.library.ui.Divider
+
+@Composable
+fun LoanHistoryScreen(){
+    val textList= listOf(
+        R.string.index, R.string.title, R.string.author,
+        R.string.loan_date, R.string.return_date, R.string.status
+    )
+    val temp= listOf(
+        "1","제목제목제목제목제목제목제목제","김저자1,이저자2,박저자3",
+        "2025-\n00-00","2025-\n00-00","반납\n완료"
+    )
+
+    UserUsageSection(textList, temp)
+}
+
+@Composable
+fun LoanStatusScreen(){
+    val textList= listOf(
+        R.string.index, R.string.title, R.string.author,
+        R.string.loan_date, R.string.return_date, R.string.status
+    )
+    val temp= listOf(
+        "1","제목제목제목제목제목제목제목제","김저자1,이저자2,박저자3",
+        "2025-\n00-00","2025-\n00-00","반납\n완료"
+    )
+
+    UserUsageSection(textList, temp){
+        LoanStatusSection()
+    }
+}
+
+@Composable
+private fun LoanStatusSection(){
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        modifier=Modifier
+            .fillMaxWidth()
+            .padding(
+                top=dimensionResource(R.dimen.padding_xl),
+                start = dimensionResource(R.dimen.padding_xs),
+                end = dimensionResource(R.dimen.padding_xs)
+        )
+    ){
+        LoanStatusSectionCard(
+            R.string.borrowed_book,
+            "2",
+            Modifier.padding(end= dimensionResource(R.dimen.padding_md))
+        )
+        LoanStatusSectionCard(
+            R.string.number_of_overdue_book,
+            "0",
+            Modifier.padding(end= dimensionResource(R.dimen.padding_md))
+        )
+        LoanStatusSectionCard(
+            R.string.suspension_end,
+            "0"
+        )
+    }
+}
+
+@Composable
+private fun LoanStatusSectionCard(
+    textTitleId:Int,
+    textContent:String,
+    modifier:Modifier=Modifier
+){
+    Card(modifier=modifier){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier=Modifier
+                .height(dimensionResource(R.dimen.loan_status_section_card_height))
+                .padding(horizontal = dimensionResource(R.dimen.padding_md))
+        ) {
+            Text(
+                stringResource(textTitleId),
+                modifier=Modifier
+                    .padding(bottom= dimensionResource(R.dimen.padding_sm)),
+                textAlign = TextAlign.Center
+            )
+            Text(textContent)
+        }
+    }
+}
+
+@Composable
+fun ReservationStatusScreen(){
+    val textList= listOf(
+        R.string.index, R.string.title, R.string.author,
+        R.string.reservation_date, R.string.reservation_rank, R.string.is_reserved
+    )
+    val temp= listOf(
+        "1","제목제목제목제목제목제목제목제","김저자1,이저자2,박저자3",
+        "2025-00-00","1","예약중"
+    )
+
+    UserUsageSection(textList, temp)
+}
+
+@Composable
+private fun UserUsageSection(
+    titleList:List<Int>,
+    contentList:List<String>,
+    weightList:List<Float> =listOf(0.1f, 0.2f, 0.2f, 0.18f, 0.18f, 0.14f),
+    extraContent:@Composable () -> Unit ={}
+){
+    Column{
+        BackIconButton(stringResource(R.string.reservation_status)){}
+        extraContent()
+        Column(
+            modifier=Modifier.padding(
+                top=dimensionResource(R.dimen.padding_lg),
+                bottom=dimensionResource(R.dimen.padding_xl),
+                start = dimensionResource(R.dimen.padding_xxl),
+                end = dimensionResource(R.dimen.padding_xxl)
+            )
+        ){
+            UserUsageList(titleList, contentList, weightList)
+        }
+    }
+}
+
+@Composable
+private fun UserUsageList(
+    titleList:List<Int>,
+    contentList:List<String>,
+    weightList:List<Float>
+){
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier=Modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(R.dimen.padding_xs))
+    ){
+        titleList.zip(weightList).forEach { (text,weight) ->
+            Text(
+                text= stringResource(text),
+                modifier=Modifier.weight(weight),
+                textAlign= TextAlign.Center
+            )
+        }
+    }
+    Divider()
+    LazyColumn {
+        items(5){
+            UserUsageListItem(contentList, weightList)
+        }
+    }
+}
+
+@Composable
+private fun UserUsageListItem(
+    contentList:List<String>,
+    weightList: List<Float>
+){
+    OutlinedCard(
+        modifier=Modifier.padding(
+            top= dimensionResource(R.dimen.padding_md)
+        )
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier=Modifier.padding(
+                horizontal = dimensionResource(R.dimen.padding_sm),
+                vertical = dimensionResource(R.dimen.padding_xs)
+            )
+        ){
+            contentList.zip(weightList).forEach{ (text, weight) ->
+                Text(
+                    text= text,
+                    modifier=Modifier.weight(weight),
+                    textAlign= TextAlign.Center
+                )
+            }
+        }
+    }
+}
