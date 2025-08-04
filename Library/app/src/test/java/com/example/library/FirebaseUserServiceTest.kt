cdd2aa7671354efc9f44bb7d71439967c5b0bf8d
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-08-04 22:36:59 +0900

    Test: Add FirebaseService register test

---
+++ Library/app/src/test/java/com/example/library/FirebaseUserServiceTest.kt
@@ -0,0 +1,66 @@
+package com.example.library
+
+import com.example.library.data.User
+import com.example.library.domain.UserRepository
+import com.example.library.fake.FakeExternalUser
+import com.example.library.fake.FakeSessionManager
+import com.example.library.service.FirebaseUserService
+import com.example.library.service.SaveUserInfoFailedException
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.coVerifySequence
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import kotlin.test.assertFailsWith
+
+class FirebaseUserServiceTest {
+
+    private val mockRepo = mockk<UserRepository>()
+    private val service = FirebaseUserService(mockRepo, FakeSessionManager())
+
+    @Test
+    fun firebaseService_register_verifySuccess()= runTest {
+        coEvery { mockRepo.createUser(any(),any()) } returns
+                Result.success(FakeExternalUser())
+        coEvery { mockRepo.saveUser(any()) } returns Result.success(Unit)
+        coEvery { mockRepo.sendVerificationEmail(any()) } returns Result.success(Unit)
+
+        service.register(User(),"")
+        coVerifySequence {
+            mockRepo.createUser(any(),any())
+            mockRepo.saveUser(any())
+            mockRepo.sendVerificationEmail(any())
+        }
+    }
+
+    @Test
+    fun firebaseService_register_verifyFailure_createUser() = runTest {
+        coEvery { mockRepo.createUser(any(),any()) } returns Result.failure(Exception())
+
+        assertFailsWith<Exception>{
+            service.register(User(),"")
+        }
+        coVerify{
+            mockRepo.createUser(any(),any())
+        }
+    }
+
+    @Test
+    fun firebaseService_register_verifyFailure_saveUser() = runTest {
+        coEvery { mockRepo.createUser(any(),any()) } returns
+                Result.success(FakeExternalUser())
+        coEvery { mockRepo.saveUser(any()) } returns Result.failure(Exception())
+        coEvery { mockRepo.deleteUserAccount(any()) } returns Result.success(Unit)
+
+        assertFailsWith<SaveUserInfoFailedException>{
+            service.register(User(),"")
+        }
+        coVerifySequence {
+            mockRepo.createUser(any(),any())
+            mockRepo.saveUser(any())
+            mockRepo.deleteUserAccount(any())
+        }
+    }
+
+}
