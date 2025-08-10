984f5c72c1db31f21c44443c644e1a909a87eb24
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-08-10 20:09:50 +0900

    Test: Create FakeUserService.kt

---
+++ Library/app/src/test/java/com/example/library/fake/FakeUserService.kt
@@ -0,0 +1,91 @@
+package com.example.library.fake
+
+import com.example.library.data.ExternalUser
+import com.example.library.data.User
+import com.example.library.domain.UserService
+import com.google.firebase.auth.FirebaseAuthException
+
+class FakeUserService:UserService {
+
+    var isThrowException= false
+    var isThrowFirebaseException= false
+
+    override suspend fun register(user: User, password: String) {
+        when{
+            isThrowException ->throw Exception("fail")
+            isThrowFirebaseException -> FirebaseAuthException("ERROR_CODE", "firebase_fail")
+            else ->{}
+        }
+    }
+
+    override suspend fun unregister(password: String) {
+        when{
+            isThrowException ->throw Exception("fail")
+            isThrowFirebaseException -> FirebaseAuthException("ERROR_CODE", "firebase_fail")
+            else ->{}
+        }
+    }
+
+    override suspend fun changeUserInfo(data: Map<String, Any>) {
+        when{
+            isThrowException ->throw Exception("fail")
+            isThrowFirebaseException -> FirebaseAuthException("ERROR_CODE", "firebase_fail")
+            else ->{}
+        }
+    }
+
+    override suspend fun signIn(email: String, password: String) {
+        when{
+            isThrowException ->throw Exception("fail")
+            isThrowFirebaseException -> FirebaseAuthException("ERROR_CODE", "firebase_fail")
+            else ->{}
+        }
+    }
+
+    override suspend fun signOut() {
+        when{
+            isThrowException ->throw Exception("fail")
+            else ->{}
+        }
+    }
+
+    override suspend fun sendVerificationEmail(user: ExternalUser?) {
+        when{
+            isThrowException ->throw Exception("fail")
+            isThrowFirebaseException -> FirebaseAuthException("ERROR_CODE", "firebase_fail")
+            else ->{}
+        }
+    }
+
+    override suspend fun isEmailVerified(): Boolean {
+        return true
+    }
+
+    override suspend fun verifyCurrentPassword(password: String) {
+        when{
+            isThrowException ->throw Exception("fail")
+            isThrowFirebaseException -> FirebaseAuthException("ERROR_CODE", "firebase_fail")
+            else ->{}
+        }
+    }
+
+    override suspend fun changePassword(password: String) {
+        when{
+            isThrowException ->throw Exception("fail")
+            isThrowFirebaseException -> FirebaseAuthException("ERROR_CODE", "firebase_fail")
+            else ->{}
+        }
+    }
+
+    override suspend fun findPassword(email: String) {
+        when{
+            isThrowException ->throw Exception("fail")
+            isThrowFirebaseException -> FirebaseAuthException("ERROR_CODE", "firebase_fail")
+            else ->{}
+        }
+    }
+
+    override suspend fun updateLogInState(b: Boolean) {
+    }
+
+}
