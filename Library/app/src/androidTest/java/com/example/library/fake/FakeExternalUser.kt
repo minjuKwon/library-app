3809419e59c45a95433a8c919d0bda5706d3bc13
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-08-26 16:09:08 +0900

    Test: Create FakeExternalUser for ui test

---
+++ Library/app/src/androidTest/java/com/example/library/fake/FakeExternalUser.kt
@@ -0,0 +1,21 @@
+package com.example.library.fake
+
+import com.example.library.data.ExternalUser
+
+class FakeExternalUser(
+    override val uid: String,
+    override val isEmailVerified: Boolean= false,
+    private var authList:MutableList<AuthUser> = mutableListOf()
+) :ExternalUser {
+
+    var isUserEmailVerified= isEmailVerified
+
+    override suspend fun delete() {
+        authList.removeIf{it.uid==uid}
+    }
+
+    override suspend fun sendEmailVerification() {
+        isUserEmailVerified=true
+    }
+
+}
