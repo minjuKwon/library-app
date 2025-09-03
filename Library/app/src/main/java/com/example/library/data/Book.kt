4d9fd201c0a970fdd23f465812def802a6e5ed9b
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-09-03 21:51:02 +0900

    Feat: Create domain entities

---
+++ Library/app/src/main/java/com/example/library/data/Book.kt
@@ -0,0 +1,29 @@
+package com.example.library.data
+
+data class Item(
+    val book:List<Book>,
+    val totalCount:Int
+)
+
+data class Book(
+    val id:String,
+    val bookInfo: BookInfo
+)
+
+data class BookInfo(
+    val title:String?=null,
+    val authors:List<String>?=null,
+    val publisher:String?=null,
+    val publishedDate:String?=null,
+    val description:String?=null,
+    val img: BookImage?=null,
+    var isBookmarked:Boolean=false
+)
+
+data class BookImage(
+    val thumbnail:String?=null,
+    val small:String?=null,
+    val medium:String?=null,
+    val large:String?=null,
+    val smallThumbnail:String?=null,
+)
