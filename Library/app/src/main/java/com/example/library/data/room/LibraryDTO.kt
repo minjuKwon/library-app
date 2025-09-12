c4123b87f1fb634b4b7ad8e1ddf3beb4a8500d4d
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-09-12 22:26:00 +0900

    Feat: Create data classes with relation mapping

---
+++ Library/app/src/main/java/com/example/library/data/room/LibraryDTO.kt
@@ -0,0 +1,33 @@
+package com.example.library.data.room
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+data class SearchResultWithLibrary(
+    @Embedded val searchResultEntity: SearchResultEntity,
+    @Relation(
+        entity = LibraryEntity::class,
+        parentColumn = "libraryId",
+        entityColumn = "libraryId"
+    )
+    val libraryBook: LibraryWithBook
+)
+
+data class LibraryWithBook(
+    @Embedded val libraryEntity:LibraryEntity,
+    @Relation(
+        entity = BookEntity::class,
+        parentColumn = "libraryId",
+        entityColumn = "id"
+    )
+    val book:BookWitImage
+)
+
+data class BookWitImage(
+    @Embedded val bookEntity:BookEntity,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "id"
+    )
+    val image:BookImageEntity
+)
