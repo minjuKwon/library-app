82d27421788952ec230b1f262c904d2139f65db2
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-09-12 22:28:20 +0900

    Feat: Create room dao

---
+++ Library/app/src/main/java/com/example/library/data/room/BookCacheDao.kt
@@ -0,0 +1,43 @@
+package com.example.library.data.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Transaction
+
+@Dao
+interface BookCacheDao {
+
+    @Insert
+    suspend fun insertSearchResult(result:SearchResultEntity)
+
+    @Insert
+    suspend fun insertLibrary(library: LibraryEntity)
+
+    @Insert
+    suspend fun insertBook(book:BookEntity)
+
+    @Insert
+    suspend fun insertBookImage(image:BookImageEntity)
+
+    @Transaction
+    suspend fun insertSearchResultWithLibrary(
+        result:SearchResultEntity,
+        library: LibraryEntity,
+        book:BookEntity,
+        image:BookImageEntity
+    ){
+        insertSearchResult(result)
+        insertLibrary(library)
+        insertBook(book)
+        insertBookImage(image)
+    }
+
+    @Transaction
+    @Query("SELECT * FROM search_result WHERE `query` = :keyword")
+    suspend fun getAllBooks(keyword:String):List<SearchResultWithLibrary>
+
+    @Query("SELECT EXISTS(SELECT 1 FROM search_result WHERE `query` = :keyword AND page=:page)")
+    suspend fun hasBooksForKeyword(keyword:String, page:Int):Boolean
+
+}
