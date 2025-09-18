73f8ed4d5ca423a2c77e955a3b957d3188575fa8
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-09-18 22:54:50 +0900

    Refactor: Update FakeNetworkBookRepository return type to use generic interface

--- Library/app/src/main/java/com/example/library/data/fake/FakeBookRepository.kt
+++ Library/app/src/main/java/com/example/library/data/fake/FakeBookRepository.kt
@@ -5,7 +5,7 @@ import com.example.library.data.mapper.toItem
 import com.example.library.domain.BookRepository
 import kotlinx.coroutines.delay
 
-class FakeNetworkBookRepository : BookRepository {
+class FakeNetworkBookRepository : BookRepository<Item> {
     override suspend fun searchVolume(query: String, limit: Int, offset: Int): Item {
         delay(3000)
         return FakeDataSource.getListRange(limit, offset).toItem()
