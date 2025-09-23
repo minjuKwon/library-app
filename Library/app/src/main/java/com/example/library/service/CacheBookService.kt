6bc9f990dc087919669cc09cfabcc1288573b46f
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-09-23 16:41:10 +0900

    Feat: Create CacheBookService

---
+++ Library/app/src/main/java/com/example/library/service/CacheBookService.kt
@@ -0,0 +1,25 @@
+package com.example.library.service
+
+import com.example.library.data.entity.Library
+import com.example.library.data.repository.CacheBookRepository
+import javax.inject.Inject
+
+class CacheBookService @Inject constructor(
+    private val cacheBookRepository: CacheBookRepository
+){
+
+    suspend fun getLibraryBooks(query: String, offset: Int, page: Int): List<Library>
+        = cacheBookRepository.searchResultData(query, offset, page)
+
+    suspend fun saveLibraryBooks(
+        library: Library,
+        query:String,
+        page:Int,
+        cachedAt:Long,
+        accessedAt:Long
+    ) = cacheBookRepository.cacheLibraryBooks(library, query, page, cachedAt, accessedAt)
+
+    suspend fun isKeywordCached(keyword: String, page: Int):Boolean
+        = cacheBookRepository.hasCachedKeyword(keyword, page)
+
+}
