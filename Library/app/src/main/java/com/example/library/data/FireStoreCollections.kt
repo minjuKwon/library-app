50bb576b87c97bf9086b04b4bfe94cc91a8584f5
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-09-24 22:21:05 +0900

    Feat: Add Firestore collection name

--- Library/app/src/main/java/com/example/library/data/FireStoreCollections.kt
+++ Library/app/src/main/java/com/example/library/data/FireStoreCollections.kt
@@ -2,4 +2,7 @@ package com.example.library.data
 
 object FireStoreCollections {
     const val USER_COLLECTION="users"
+    const val SEARCH_RESULTS_COLLECTION = "search_results"
+    const val PAGE_NUMBER_COLLECTION = "page_number"
+    const val LIBRARY_COLLECTION= "library"
 }
