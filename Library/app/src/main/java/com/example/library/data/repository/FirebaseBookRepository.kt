0b67dd0d59f5b26b17d6f170af5851e03224d108
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-09-25 16:37:56 +0900

    Feat: Create FirebaseBookRepository

---
+++ Library/app/src/main/java/com/example/library/data/repository/FirebaseBookRepository.kt
@@ -0,0 +1,58 @@
+package com.example.library.data.repository
+
+import com.example.library.data.FireStoreCollections.LIBRARY_COLLECTION
+import com.example.library.data.FireStoreCollections.PAGE_NUMBER_COLLECTION
+import com.example.library.data.FireStoreCollections.SEARCH_RESULTS_COLLECTION
+import com.example.library.data.entity.Library
+import com.example.library.domain.DatabaseRepository
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
+
+class FirebaseBookRepository@Inject constructor(
+    private val fireStore: FirebaseFirestore
+): DatabaseRepository {
+
+    override suspend fun addLibraryBook(keyword: String, page: String, list: List<Library>) {
+        val batch = fireStore.batch()
+
+        list.forEach { item ->
+            val docRef= fireStore.collection(SEARCH_RESULTS_COLLECTION)
+                .document(keyword)
+                .collection(PAGE_NUMBER_COLLECTION)
+                .document(page)
+                .collection(LIBRARY_COLLECTION)
+                .document(item.libraryId)
+            batch.set(docRef, item)
+        }
+
+        batch.commit().await()
+    }
+
+    override suspend fun getLibraryBook(keyword: String, page: String): List<Library> {
+        val snapshot= fireStore.collection(SEARCH_RESULTS_COLLECTION)
+            .document(keyword)
+            .collection(PAGE_NUMBER_COLLECTION)
+            .document(page)
+            .collection(LIBRARY_COLLECTION)
+            .orderBy("offset")
+            .get()
+            .await()
+
+        return snapshot.documents.map { doc ->
+            doc.toObject(Library::class.java)!!
+        }
+    }
+
+    override suspend fun hasServerBook(keyword: String, page: String): Boolean {
+        val snapshot = fireStore.collection(SEARCH_RESULTS_COLLECTION)
+            .document(keyword)
+            .collection(PAGE_NUMBER_COLLECTION)
+            .document(page)
+            .get()
+            .await()
+
+        return snapshot.exists()
+    }
+
+}
