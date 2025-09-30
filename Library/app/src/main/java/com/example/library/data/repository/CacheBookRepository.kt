9863b8c5df0e81e143dad27ee61ff9131a24d8f3
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-09-30 18:05:07 +0900

    Feat: Add get/save methods for totalCount in repository

--- Library/app/src/main/java/com/example/library/data/repository/CacheBookRepository.kt
+++ Library/app/src/main/java/com/example/library/data/repository/CacheBookRepository.kt
@@ -8,6 +8,7 @@ import com.example.library.data.mapper.toLibraryEntity
 import com.example.library.data.room.BookCacheDao
 import com.example.library.data.mapper.toListLibrary
 import com.example.library.data.mapper.toSearchResultEntity
+import com.example.library.data.room.SearchTotalCountEntity
 import com.example.library.domain.LocalRepository
 import javax.inject.Inject
 
@@ -19,7 +20,6 @@ class CacheBookRepository @Inject constructor(
         val normalizeQuery= normalizeQuery(query)
         return bookCacheDao.getBooks(normalizeQuery, page).toListLibrary(offset)
     }
-
 
     override suspend fun cacheLibraryBooks(
         library: Library,
@@ -36,6 +36,18 @@ class CacheBookRepository @Inject constructor(
             library.book.bookInfo.toBookEntity(library.book.id),
             library.book.bookInfo.img.toBookImageEntity(library.book.id)
         )
+    }
+
+    override suspend fun searchTotalCount(query: String): Int? {
+        val normalizeQuery= normalizeQuery(query)
+        return bookCacheDao.getSearchTotalCount(normalizeQuery)
+    }
+
+    override suspend fun cacheTotalCount(query: String, count: Int) {
+        val normalizeQuery= normalizeQuery(query)
+        val entity= SearchTotalCountEntity(normalizeQuery, count)
+
+        bookCacheDao.insertSearchTotalCount(entity)
     }
 
     override suspend fun hasCachedKeyword(keyword: String, page: Int):Boolean{
