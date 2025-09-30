82473a82f9a3e53b5392b5faa58a94afa4a895ce
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-09-30 18:03:46 +0900

    Feat: Add get/save methods for totalCount in repository interface

--- Library/app/src/main/java/com/example/library/domain/LocalRepository.kt
+++ Library/app/src/main/java/com/example/library/domain/LocalRepository.kt
@@ -5,5 +5,7 @@ import com.example.library.data.entity.Library
 interface LocalRepository {
     suspend fun searchResultData(query: String, offset: Int, page: Int): List<Library>
     suspend fun cacheLibraryBooks(library: Library, query:String, page:Int, cachedAt:Long, accessedAt:Long)
+    suspend fun searchTotalCount(query: String): Int?
+    suspend fun cacheTotalCount(query:String, count:Int)
     suspend fun hasCachedKeyword(keyword: String, page: Int):Boolean
 }
