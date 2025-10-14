f14141b6a1257d4a299402a431125a8de5c51bd3
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-10-14 22:19:39 +0900

    Feat: Create LibrarySyncService interface

---
+++ Library/app/src/main/java/com/example/library/domain/LibrarySyncService.kt
@@ -0,0 +1,17 @@
+package com.example.library.domain
+
+import com.example.library.data.entity.Library
+
+interface LibrarySyncService {
+    suspend fun getSearchBooks(getSearchBooksCommands: GetSearchBooksCommands):List<Library>?
+    suspend fun getTotalCntForKeyword(keyword:String):Int?
+}
+
+data class GetSearchBooksCommands(
+    val keyword:String,
+    val limit:Int,
+    val offset:Int,
+    val pageNumber:Int,
+    val cachedAt:Long,
+    val accessedAt:Long
+)
