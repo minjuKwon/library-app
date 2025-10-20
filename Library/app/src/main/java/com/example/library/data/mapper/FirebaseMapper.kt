f75b703f4735456e8e78aa7edd360a602d39588b
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-10-20 21:22:18 +0900

    Feat: Create FirebaseMapper

---
+++ Library/app/src/main/java/com/example/library/data/mapper/FirebaseMapper.kt
@@ -0,0 +1,40 @@
+package com.example.library.data.mapper
+
+import com.example.library.data.entity.BookStatus
+import com.example.library.data.entity.Library
+import com.example.library.data.firebase.LibraryFirebaseDto
+import java.time.Instant
+
+fun Library.toFirebaseDto():LibraryFirebaseDto= LibraryFirebaseDto(
+    libraryId= this.libraryId,
+    book = this.book,
+    statusType= this.bookStatus.toStringType(),
+    userEmail= this.bookStatus.toStringEmail(),
+    borrowedAt= this.bookStatus.toLongBorrowedAt(),
+    reservedAt= this.bookStatus.toLongReservedAt(),
+    callNumber= this.callNumber,
+    location = this.location,
+    offset = this.offset
+)
+
+fun LibraryFirebaseDto.toLibrary(): Library= Library(
+    libraryId= this.libraryId,
+    book = this.book,
+    bookStatus = toBookStatus(this),
+    callNumber= this.callNumber,
+    location = this.location,
+    offset = this.offset
+)
+
+fun toBookStatus(libraryFirebaseDto: LibraryFirebaseDto) = when(libraryFirebaseDto.statusType){
+    "Available" -> BookStatus.Available
+    "Borrowed" -> BookStatus.Borrowed(
+        libraryFirebaseDto.userEmail!!,
+        Instant.ofEpochMilli(libraryFirebaseDto.borrowedAt!!)
+    )
+    "Reserved" -> BookStatus.Reserved(
+        libraryFirebaseDto.userEmail!!,
+        Instant.ofEpochMilli(libraryFirebaseDto.reservedAt!!)
+    )
+    else -> throw IllegalArgumentException("Unknown bookStatus: ${libraryFirebaseDto.statusType}")
+}
