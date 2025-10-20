a747a623910b7a865f1464dafd974da781899f0f
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-10-20 22:02:42 +0900

    Fix: Update condition to check data stored in firestore

--- Library/app/src/main/java/com/example/library/service/DefaultLibrarySyncService.kt
+++ Library/app/src/main/java/com/example/library/service/DefaultLibrarySyncService.kt
@@ -28,7 +28,7 @@ class DefaultLibrarySyncService @Inject constructor(
             val firebaseResult= isSavedFirebase.getOrNull()
             firebaseResult?.let {
                 //캐시된 데이터가 없지만 firebase 저장된 데이터가 있으면 가져오고 캐시하기
-                if(firebaseResult){
+                if(!firebaseResult){
                     val isGetList= firebaseBookService
                         .getLibraryBooks(keyword, strPage)
                     if(isGetList.isFailure) throw isGetList.exceptionOrNull()?:GetLibraryInfoFailedException()
