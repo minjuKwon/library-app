49d370847f185ad4c57af7e90de752c2d500242f
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-10-28 15:25:28 +0900

    Feat: Create FakeLibrarySyncService in ui test

---
+++ Library/app/src/androidTest/java/com/example/library/fake/FakeLibrarySyncService.kt
@@ -0,0 +1,11 @@
+package com.example.library.fake
+
+import com.example.library.data.entity.Library
+import com.example.library.domain.LibrarySyncService
+
+class FakeLibrarySyncService:LibrarySyncService {
+    override suspend fun getSearchBooks(keyword: String, pageNumber: Int): List<Library>? =
+        emptyList()
+
+    override suspend fun getTotalCntForKeyword(keyword: String): Int? = 0
+}
