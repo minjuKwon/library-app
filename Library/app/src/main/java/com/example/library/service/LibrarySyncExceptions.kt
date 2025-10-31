febcadcdf78d57d51a328e46f321f3183eecdac3
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-10-31 14:53:28 +0900

    Test: Create CheckLibraryInfoFailedException

--- Library/app/src/main/java/com/example/library/service/LibrarySyncExceptions.kt
+++ Library/app/src/main/java/com/example/library/service/LibrarySyncExceptions.kt
@@ -1,5 +1,6 @@
 package com.example.library.service
 
+class CheckLibraryInfoFailedException:Exception("도서 정보 저장 확인 실패")
 class SaveLibraryInfoFailedException:Exception("도서 정보 저장 실패")
 class GetLibraryInfoFailedException:Exception("도서 정보 가져오기 실패")
 class GetBookFailedException:Exception("책 정보 가져오기 실패")
