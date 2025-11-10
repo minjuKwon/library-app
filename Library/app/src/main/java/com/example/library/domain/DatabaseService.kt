d5a8ff66fd9da2b188ba0e97749b3aedfc6daa87
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-11-10 18:17:51 +0900

    Feat: Add liked method in DatabaseService

--- Library/app/src/main/java/com/example/library/domain/DatabaseService.kt
+++ Library/app/src/main/java/com/example/library/domain/DatabaseService.kt
@@ -1,9 +1,12 @@
 package com.example.library.domain
 
 import com.example.library.data.entity.Library
+import com.example.library.data.entity.LibraryLiked
 
 interface DatabaseService {
     suspend fun saveLibraryBooks(keyword:String, page: String, list:List<Library>):Result<Unit>
     suspend fun getLibraryBooks(keyword:String, page: String):Result<List<Library>>
     suspend fun isSavedBook(keyword:String, page:String):Result<Boolean>
+    suspend fun updateLibraryLiked(userId:String, bookId:String, isLiked:Boolean):Result<List<LibraryLiked>>
+    suspend fun getLibraryLiked(userId:String):Result<List<LibraryLiked>>
 }
