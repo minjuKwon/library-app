d9d04eb9e8f3a4c6f76215f4cd4759d43e9be0b2
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-11-10 17:31:45 +0900

    Feat: Add LIBRARY_LIKED in firestore collection

--- Library/app/src/main/java/com/example/library/data/FireStoreCollections.kt
+++ Library/app/src/main/java/com/example/library/data/FireStoreCollections.kt
@@ -5,4 +5,5 @@ object FireStoreCollections {
     const val SEARCH_RESULTS_COLLECTION = "search_results"
     const val PAGE_NUMBER_COLLECTION = "page_number"
     const val LIBRARY_COLLECTION= "library"
+    const val LIBRARY_LIKED="library_liked"
 }
