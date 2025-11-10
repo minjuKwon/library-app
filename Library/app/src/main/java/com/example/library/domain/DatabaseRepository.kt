6fd5799c56c330819c4487d7e61e551281cd49b5
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-11-10 17:57:56 +0900

    Feat: Add liked method in DatabaseRepository

--- Library/app/src/main/java/com/example/library/domain/DatabaseRepository.kt
+++ Library/app/src/main/java/com/example/library/domain/DatabaseRepository.kt
@@ -1,9 +1,14 @@
 package com.example.library.domain
 
 import com.example.library.data.entity.Library
+import com.example.library.data.entity.LibraryLiked
 
 interface DatabaseRepository {
     suspend fun addLibraryBook(keyword:String, page: String, list:List<Library>):Result<Unit>
     suspend fun getLibraryBook(keyword:String, page: String):Result<List<Library>>
     suspend fun hasServerBook(keyword:String, page:String):Result<Boolean>
+    suspend fun addLibraryLiked(libraryLiked: LibraryLiked):Result<Unit>
+    suspend fun updateLibraryLiked(id:String, data: Map<String, Any>):Result<Unit>
+    suspend fun getLibraryLiked(userId:String):Result<List<LibraryLiked>>
+    suspend fun hasLibraryLiked(id:String):Result<Boolean>
 }
