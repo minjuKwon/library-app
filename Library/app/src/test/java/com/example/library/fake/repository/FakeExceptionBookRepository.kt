e1e8434b49fbfc44eb552379b32e3660043804dc
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-11-22 00:24:37 +0900

    Test: Create FakeExceptionBookRepository

---
+++ Library/app/src/test/java/com/example/library/fake/repository/FakeExceptionBookRepository.kt
@@ -0,0 +1,109 @@
+package com.example.library.fake.repository
+
+import com.example.library.data.QueryNormalizer.normalizeQuery
+import com.example.library.data.entity.Library
+import com.example.library.data.entity.LibraryLiked
+import com.example.library.domain.DatabaseRepository
+import com.example.library.service.CheckLibraryLikeFailedException
+import com.google.firebase.firestore.ListenerRegistration
+
+class FakeExceptionBookRepository:DatabaseRepository {
+
+    private data class DatabaseItem(
+        val query:String,
+        val page:String,
+        val library: Library
+    )
+
+    private val itemList= mutableListOf<DatabaseItem>()
+    private val likeList= mutableListOf<LibraryLiked>()
+
+    override suspend fun addLibraryBook(
+        keyword: String,
+        page: String,
+        list: List<Library>
+    ): Result<Unit> {
+        return try {
+            val normalizedQuery= normalizeQuery(keyword)
+            list.forEach { item ->
+                val data= DatabaseItem(normalizedQuery, page, item)
+                itemList.add(data)
+            }
+
+            Result.success(Unit)
+        }catch(e:Exception){
+            Result.failure(e)
+        }
+    }
+
+    override suspend fun getLibraryBook(keyword: String, page: String): Result<List<Library>> {
+        return try {
+            val normalizedQuery= normalizeQuery(keyword)
+            val list= itemList
+                .filter { it.query==normalizedQuery && it.page==page }
+                .sortedBy { it.library.offset }
+                .map { it.library }
+
+            Result.success(list)
+        }catch(e:Exception){
+            Result.failure(e)
+        }
+    }
+
+    override suspend fun hasServerBook(keyword: String, page: String): Result<Boolean> {
+        return try {
+            val normalizedQuery= normalizeQuery(keyword)
+            val isSave= itemList.any { it.query==normalizedQuery && it.page==page }
+
+            Result.success(isSave)
+        }catch(e:Exception){
+            Result.failure(e)
+        }
+    }
+
+    override suspend fun addLibraryLiked(libraryLiked: LibraryLiked): Result<Unit> {
+        return try {
+            likeList.add(libraryLiked)
+
+            Result.success(Unit)
+        }catch(e:Exception){
+            Result.failure(e)
+        }
+    }
+
+    override suspend fun updateLibraryLiked(id: String, data: Map<String, Any>): Result<Unit> {
+        return try {
+            val index= likeList.indexOfFirst { it.likedId==id }
+            likeList[index]= likeList[index].copy(
+                isLiked = (data["isLiked"] as Boolean),
+                timestamp = (data["timestamp"] as Number).toLong()
+            )
+            Result.success(Unit)
+        }catch(e:Exception){
+            Result.failure(e)
+        }
+    }
+
+    override suspend fun getLibraryLikedList(userId: String): Result<List<LibraryLiked>> {
+        return try {
+            Result.success(likeList)
+        }catch(e:Exception){
+            Result.failure(e)
+        }
+    }
+
+    override fun getLibraryLikedCount(
+        bookId: String,
+        onUpdate: (Int) -> Unit
+    ): ListenerRegistration {
+        val count= likeList.count { it.bookId==bookId }
+        onUpdate(count)
+
+        return ListenerRegistration { }
+    }
+
+    override suspend fun hasLibraryLiked(id: String): Result<Boolean> {
+        throw CheckLibraryLikeFailedException()
+    }
+
+}
