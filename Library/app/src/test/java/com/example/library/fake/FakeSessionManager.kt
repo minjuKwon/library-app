d7d8cb5aa1b0288574562dfb611788b76001dd11
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-11-21 20:49:12 +0900

    Test: Update userPreferences initial value due to structural change

--- Library/app/src/test/java/com/example/library/fake/FakeSessionManager.kt
+++ Library/app/src/test/java/com/example/library/fake/FakeSessionManager.kt
@@ -4,9 +4,10 @@ import com.example.library.domain.SessionManager
 import com.example.library.data.entity.User
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flowOf
 
 class FakeSessionManager: SessionManager {
-    override val userPreferences: Flow<User> = emptyFlow()
+    override val userPreferences: Flow<User> = flowOf(User())
     override val logInPreferences: Flow<Boolean> = emptyFlow()
 
     override suspend fun saveUserData(user: User) {
