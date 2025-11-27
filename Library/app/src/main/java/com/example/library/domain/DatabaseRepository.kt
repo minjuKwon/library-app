7fb531f0aa413844463ab58a761d7dde79a57eda
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-11-27 18:41:20 +0900

    Feat: Add loan methods in DatabaseRepository interface

--- Library/app/src/main/java/com/example/library/domain/DatabaseRepository.kt
+++ Library/app/src/main/java/com/example/library/domain/DatabaseRepository.kt
@@ -1,11 +1,18 @@
 package com.example.library.domain
 
 import com.example.library.data.entity.Library
+import com.example.library.data.entity.LibraryHistory
 import com.example.library.data.entity.LibraryLiked
 import com.google.firebase.firestore.ListenerRegistration
 
 interface DatabaseRepository {
     suspend fun addLibraryBook(keyword:String, page: String, list:List<Library>):Result<Unit>
+    suspend fun updateLibraryBook(
+        libraryId:String,
+        keyword:String,
+        page: String,
+        data: Map<String, Any>
+    ):Result<Unit>
     suspend fun getLibraryBook(keyword:String, page: String):Result<List<Library>>
     suspend fun hasServerBook(keyword:String, page:String):Result<Boolean>
     suspend fun addLibraryLiked(libraryLiked: LibraryLiked):Result<Unit>
@@ -13,4 +20,5 @@ interface DatabaseRepository {
     suspend fun getLibraryLikedList(userId:String):Result<List<LibraryLiked>>
     fun getLibraryLikedCount(bookId: String, onUpdate: (Int) -> Unit):ListenerRegistration
     suspend fun hasLibraryLiked(id:String):Result<Boolean>
+    suspend fun addLoanHistory(libraryHistory: LibraryHistory):Result<Unit>
 }
