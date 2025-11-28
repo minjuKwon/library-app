d35be77c3628d8ea70522e899049f9e575437785
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-11-28 22:58:50 +0900

    Feat: Add toBookStatus() in LibraryHistory

--- Library/app/src/main/java/com/example/library/data/mapper/FirebaseMapper.kt
+++ Library/app/src/main/java/com/example/library/data/mapper/FirebaseMapper.kt
@@ -3,6 +3,7 @@ package com.example.library.data.mapper
 import com.example.library.data.entity.BookStatus
 import com.example.library.data.entity.BookStatusType
 import com.example.library.data.entity.Library
+import com.example.library.data.entity.LibraryHistory
 import com.example.library.data.firebase.LibraryFirebaseDto
 import java.time.Instant
 
@@ -39,4 +40,15 @@ fun toBookStatus(libraryFirebaseDto: LibraryFirebaseDto) = when(libraryFirebaseD
         Instant.ofEpochMilli(libraryFirebaseDto.reservedAt!!)
     )
     else -> throw IllegalArgumentException("Unknown bookStatus: ${libraryFirebaseDto.statusType}")
+}
+
+fun LibraryHistory.toBookStatus():BookStatus {
+    return when(this.status){
+        BookStatusType.AVAILABLE.name -> BookStatus.Available
+        BookStatusType.BORROWED.name->
+            BookStatus.Borrowed(this.userId,Instant.ofEpochMilli(this.loanDate), Instant.ofEpochMilli(this.dueDate))
+        BookStatusType.RESERVED.name->
+            BookStatus.Reserved(this.userId, Instant.ofEpochMilli(this.loanDate))
+        else -> BookStatus.Available
+    }
 }
