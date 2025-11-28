7196ad56770175da664f5b11528227e7d647ea04
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-11-28 17:41:23 +0900

    Feat: Add loanLibrary() in LibraryDetailsViewModel

--- Library/app/src/main/java/com/example/library/ui/screens/detail/LibraryDetailsViewModel.kt
+++ Library/app/src/main/java/com/example/library/ui/screens/detail/LibraryDetailsViewModel.kt
@@ -4,13 +4,39 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.library.data.entity.Library
+import com.example.library.data.entity.User
+import com.example.library.di.ApplicationScope
+import com.example.library.domain.SessionManager
+import com.example.library.service.FirebaseBookService
 import com.example.library.ui.screens.search.defaultLibrary
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class LibraryDetailsViewModel @Inject constructor(): ViewModel() {
+class LibraryDetailsViewModel @Inject constructor(
+    private val firebaseBookService: FirebaseBookService,
+    defaultSessionManager: SessionManager,
+    @ApplicationScope externalScope: CoroutineScope? = null
+): ViewModel() {
+
+    private val scope = externalScope ?: viewModelScope
+
+    private val _userPreferences: StateFlow<User?> =
+        defaultSessionManager.userPreferences.stateIn(
+            viewModelScope,
+            SharingStarted.Eagerly,
+            null
+        )
+    val userPreferences= _userPreferences
 
     var uiState: LibraryDetailsUiState by mutableStateOf(LibraryDetailsUiState.Loading)
         private set
@@ -22,6 +48,34 @@ class LibraryDetailsViewModel @Inject constructor(): ViewModel() {
         uiState=LibraryDetailsUiState.Loading
         _currentLibrary.value= library
         uiState=LibraryDetailsUiState.Success
+    }
+
+    fun loanLibrary(
+        keyword: String,
+        page: String
+    ){
+        uiState=LibraryDetailsUiState.Loading
+
+        scope.launch {
+            val id= awaitUserId()
+            uiState=try{
+                val isSave= firebaseBookService.saveLoanHistory(
+                    userId = id,
+                    keyword = keyword,
+                    page = page,
+                    libraryId = _currentLibrary.value.libraryId,
+                    bookId = _currentLibrary.value.book.id
+                )
+                if(isSave.isSuccess)LibraryDetailsUiState.Success
+                else LibraryDetailsUiState.Error
+            }catch(e: Exception){
+                LibraryDetailsUiState.Error
+            }
+        }
+    }
+
+    private suspend fun awaitUserId(): String {
+        return userPreferences.filterNotNull().first().uid
     }
 
 }
