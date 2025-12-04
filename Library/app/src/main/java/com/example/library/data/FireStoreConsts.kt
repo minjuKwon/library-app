5f6b20e89f2b96e76b8e238da895147dc49d9fdf
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-12-04 22:26:05 +0900

    Feat: Add USER_LOAN_LIBRARY_COLLECTION in FireStoreCollections

--- Library/app/src/main/java/com/example/library/data/FireStoreConsts.kt
+++ Library/app/src/main/java/com/example/library/data/FireStoreConsts.kt
@@ -2,6 +2,7 @@ package com.example.library.data
 
 object FireStoreCollections {
     const val USER_COLLECTION="users"
+    const val USER_LOAN_LIBRARY_COLLECTION="user_loan_library"
     const val SEARCH_RESULTS_COLLECTION = "search_results"
     const val PAGE_NUMBER_COLLECTION = "page_number"
     const val LIBRARY_COLLECTION= "library"
