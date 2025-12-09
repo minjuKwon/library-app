bb54e8ea25e57e28d3482eae13d6f1821511342f
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-12-09 21:38:40 +0900

    Feat: Add GetLoanDueStatusFailedException in FirebaseBookServiceExceptions

--- Library/app/src/main/java/com/example/library/service/FirebaseBookServiceExceptions.kt
+++ Library/app/src/main/java/com/example/library/service/FirebaseBookServiceExceptions.kt
@@ -2,3 +2,4 @@ package com.example.library.service
 
 class CheckLibraryLikeFailedException:Exception("도서 좋아요 저장 확인 실패")
 class UpdateLibraryStatusFailedException:Exception("도서 상태 수정 실패")
+class GetLoanDueStatusFailedException:Exception("도서 대출일 상태 확인 실패")
