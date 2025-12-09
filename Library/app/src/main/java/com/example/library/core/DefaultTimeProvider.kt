d2606f2b9ff5e2f2ac13269c719df7c1b6302265
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-12-09 21:18:49 +0900

    Feat: Add getLocalDate() implementation in DefaultTimeProvider

--- Library/app/src/main/java/com/example/library/core/DefaultTimeProvider.kt
+++ Library/app/src/main/java/com/example/library/core/DefaultTimeProvider.kt
@@ -1,5 +1,17 @@
 package com.example.library.core
 
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
+
 class DefaultTimeProvider:TimeProvider {
     override fun now(): Long = System.currentTimeMillis()
+
+    override fun getLocalDate(currentMillis: Long, zoneId:String):LocalDate{
+        val seoulZone = ZoneId.of(zoneId)
+        val loanDateTime = Instant.ofEpochMilli(currentMillis).atZone(seoulZone)
+
+        return loanDateTime.toLocalDate()
+    }
+
 }
