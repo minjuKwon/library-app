8df20bbeec2082af30c1ee909a098527508434f7
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-12-09 21:15:16 +0900

    Feat: Add getLocalDate() in TimeProvider Interface

--- Library/app/src/main/java/com/example/library/core/TimeProvider.kt
+++ Library/app/src/main/java/com/example/library/core/TimeProvider.kt
@@ -1,5 +1,8 @@
 package com.example.library.core
 
+import java.time.LocalDate
+
 interface TimeProvider {
     fun now():Long
+    fun getLocalDate(currentMillis: Long, zoneId:String="Asia/Seoul"): LocalDate
 }
