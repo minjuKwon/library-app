934cad88dc88e66d8cea5b71cb46707019db2796
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-12-10 22:23:42 +0900

    Feat: Add calculateOverDueDate in TimeProvider Interface

--- Library/app/src/main/java/com/example/library/core/TimeProvider.kt
+++ Library/app/src/main/java/com/example/library/core/TimeProvider.kt
@@ -5,5 +5,6 @@ import java.time.LocalDate
 interface TimeProvider {
     fun now():Long
     fun calculateDueDate(currentMillis: Long, zoneId:String="Asia/Seoul"): Long
+    fun calculateOverDueDate(currentMillis: Long, zoneId:String="Asia/Seoul"): Long
     fun getLocalDate(currentMillis: Long, zoneId:String="Asia/Seoul"): LocalDate
 }
