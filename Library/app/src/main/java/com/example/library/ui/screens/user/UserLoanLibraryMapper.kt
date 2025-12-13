d210df83da5ada7b1cb2eb4b08c4739a41619000
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-12-13 21:20:01 +0900

    Feat: Add book status condition in UserLoanLibraryMapper

--- Library/app/src/main/java/com/example/library/ui/screens/user/UserLoanLibraryMapper.kt
+++ Library/app/src/main/java/com/example/library/ui/screens/user/UserLoanLibraryMapper.kt
@@ -55,7 +55,9 @@ fun List<UserLoanLibrary>.toOverdueStringList(): List<List<String>>{
 fun List<UserLoanLibrary>.getSuspensionEndDate(): String{
     var minDate:Long= Long.MAX_VALUE
     this.forEach {userLoanLibrary ->
-        minDate= minOf(minDate, userLoanLibrary.dueDate)
+        if(userLoanLibrary.returnDate>userLoanLibrary.dueDate){
+            minDate= minOf(minDate, userLoanLibrary.dueDate)
+        }
     }
 
     return formatSuspensionEndDate(minDate)
@@ -75,16 +77,18 @@ fun List<UserLoanLibrary>.toUserHistoryStringList(): List<List<String>>{
     val resultList:MutableList<List<String>> = mutableListOf()
 
     this.forEachIndexed { index, userLoanLibrary ->
-        val author= userLoanLibrary.authors?.joinToString(",")?:""
-        val list=listOf(
-            (this.size-index).toString(),
-            userLoanLibrary.title?:"",
-            author,
-            formatDateOnly(userLoanLibrary.loanDate),
-            formatDateOnly(userLoanLibrary.returnDate),
-        )
+        if(userLoanLibrary.status==BookStatusType.RETURNED.name){
+            val author= userLoanLibrary.authors?.joinToString(",")?:""
+            val list=listOf(
+                (this.size-index).toString(),
+                userLoanLibrary.title?:"",
+                author,
+                formatDateOnly(userLoanLibrary.loanDate),
+                formatDateOnly(userLoanLibrary.returnDate),
+            )
 
-        resultList.add(list)
+            resultList.add(list)
+        }
     }
     return resultList.toList()
 }
