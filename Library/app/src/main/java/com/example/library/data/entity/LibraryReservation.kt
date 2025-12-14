5a99362df62fc2bdfc0e345ad8e30ae2e1bd3708
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-12-14 23:42:10 +0900

    Feat: Create LibraryReservation data class

---
+++ Library/app/src/main/java/com/example/library/data/entity/LibraryReservation.kt
@@ -0,0 +1,15 @@
+package com.example.library.data.entity
+
+data class LibraryReservation(
+    val libraryReservationId:String="",
+    val userId:String="",
+    val bookId:String="",
+    val reservedAt:Long=0,
+    val status:String=""
+)
+
+enum class ReservationStatusType(val ko:String){
+    WAITING("예약 대기 중"),
+    NOTIFIED("예약 안내 완료"),
+    CANCELLED("예약 취소"),
+}
