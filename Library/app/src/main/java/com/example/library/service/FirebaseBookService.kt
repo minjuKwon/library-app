b42802e3c38e6c7813f1f1a27184efcefab95104
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-12-17 14:44:45 +0900

    Feat: Add isReservedBook() in FirebaseBookService

--- Library/app/src/main/java/com/example/library/service/FirebaseBookService.kt
+++ Library/app/src/main/java/com/example/library/service/FirebaseBookService.kt
@@ -180,6 +180,10 @@ class FirebaseBookService@Inject constructor(
         return databaseRepository.hasOverdueBook(userId)
     }
 
+    override suspend fun isReservedBook(userId: String): Result<Boolean> {
+        return databaseRepository.hasReservationBook(userId)
+    }
+
     override suspend fun getLibraryReservationCount(bookId: String): Result<Int> {
         return databaseRepository.getLibraryReservationCount(bookId)
     }
