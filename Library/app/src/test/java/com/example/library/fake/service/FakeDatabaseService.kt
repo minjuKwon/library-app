6662216567e5d0351e6b816a245341063de5bc82
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-12-21 16:04:52 +0900

    Test: Add override method in FakeDatabaseService

--- Library/app/src/test/java/com/example/library/fake/service/FakeDatabaseService.kt
+++ Library/app/src/test/java/com/example/library/fake/service/FakeDatabaseService.kt
@@ -1,8 +1,12 @@
 package com.example.library.fake.service
 
 import com.example.library.data.entity.Library
+import com.example.library.data.entity.LibraryHistory
 import com.example.library.data.entity.LibraryLiked
+import com.example.library.data.entity.LibraryReservation
+import com.example.library.data.entity.UserLoanLibrary
 import com.example.library.domain.DatabaseService
+import com.example.library.domain.DueCheckResult
 import com.example.library.service.CheckLibraryInfoFailedException
 import com.example.library.service.GetLibraryInfoFailedException
 import com.example.library.service.SaveLibraryInfoFailedException
@@ -43,6 +47,71 @@ class FakeSaveDatabaseService:DatabaseService {
     ): ListenerRegistration {
         return ListenerRegistration { }
     }
+
+    override fun getLibraryStatus(
+        bookId: String,
+        callback: (LibraryHistory) -> Unit
+    ): ListenerRegistration {
+        throw FakeSaveDatabaseServiceFailedException()
+    }
+
+    override suspend fun updateLibraryHistory(
+        userId: String,
+        libraryId: String,
+        bookId: String,
+        bookStatus: String,
+        bookTitle: String?,
+        bookAuthors: List<String>?,
+        keyword: String,
+        page: String
+    ): Result<Unit> {
+        throw FakeSaveDatabaseServiceFailedException()
+    }
+
+    override suspend fun getUserLoanBookList(userId: String): Result<List<UserLoanLibrary>> {
+        throw FakeSaveDatabaseServiceFailedException()
+    }
+
+    override suspend fun getUserLoanHistoryList(userId: String): Result<List<UserLoanLibrary>> {
+        throw FakeSaveDatabaseServiceFailedException()
+    }
+
+    override suspend fun getLoanDueStatus(
+        userId: String,
+        keyword: String,
+        page: String
+    ): Result<DueCheckResult> {
+        throw FakeSaveDatabaseServiceFailedException()
+    }
+
+    override suspend fun isOverdueBook(userId: String): Result<Boolean> {
+        throw FakeSaveDatabaseServiceFailedException()
+    }
+
+    override suspend fun isReservedBook(bookId: String): Result<Boolean> {
+        throw FakeSaveDatabaseServiceFailedException()
+    }
+
+    override suspend fun isUserReservedBook(userId: String): Result<Boolean> {
+        throw FakeSaveDatabaseServiceFailedException()
+    }
+
+    override suspend fun getLibraryReservationCount(bookId: String): Result<Int> {
+        throw FakeSaveDatabaseServiceFailedException()
+    }
+
+    override suspend fun getReservedStatus(userId: String, bookId: String): Result<String> {
+        throw FakeSaveDatabaseServiceFailedException()
+    }
+
+    override suspend fun checkMyReservationTurn(userId: String): Result<List<LibraryReservation>> {
+        throw FakeSaveDatabaseServiceFailedException()
+    }
+
+    override suspend fun getReservationList(userId: String): Result<List<List<String>>> {
+        throw FakeSaveDatabaseServiceFailedException()
+    }
+
 }
 
 class FakeGetDatabaseService:DatabaseService {
@@ -79,7 +148,71 @@ class FakeGetDatabaseService:DatabaseService {
         onUpdate: (Int) -> Unit
     ): ListenerRegistration {
         return ListenerRegistration { }
+
+    override fun getLibraryStatus(
+        bookId: String,
+        callback: (LibraryHistory) -> Unit
+    ): ListenerRegistration {
+        throw FakeGetDatabaseServiceFailedException()
     }
+
+    override suspend fun updateLibraryHistory(
+        userId: String,
+        libraryId: String,
+        bookId: String,
+        bookStatus: String,
+        bookTitle: String?,
+        bookAuthors: List<String>?,
+        keyword: String,
+        page: String
+    ): Result<Unit> {
+        throw FakeGetDatabaseServiceFailedException()
+    }
+
+    override suspend fun getUserLoanBookList(userId: String): Result<List<UserLoanLibrary>> {
+        throw FakeGetDatabaseServiceFailedException()
+    }
+
+    override suspend fun getUserLoanHistoryList(userId: String): Result<List<UserLoanLibrary>> {
+        throw FakeGetDatabaseServiceFailedException()
+    }
+
+    override suspend fun getLoanDueStatus(
+        userId: String,
+        keyword: String,
+        page: String
+    ): Result<DueCheckResult> {
+        throw FakeGetDatabaseServiceFailedException()
+    }
+
+    override suspend fun isOverdueBook(userId: String): Result<Boolean> {
+        throw FakeGetDatabaseServiceFailedException()
+    }
+
+    override suspend fun isReservedBook(bookId: String): Result<Boolean> {
+        throw FakeGetDatabaseServiceFailedException()
+    }
+
+    override suspend fun isUserReservedBook(userId: String): Result<Boolean> {
+        throw FakeGetDatabaseServiceFailedException()
+    }
+
+    override suspend fun getLibraryReservationCount(bookId: String): Result<Int> {
+        throw FakeGetDatabaseServiceFailedException()
+    }
+
+    override suspend fun getReservedStatus(userId: String, bookId: String): Result<String> {
+        throw FakeGetDatabaseServiceFailedException()
+    }
+
+    override suspend fun checkMyReservationTurn(userId: String): Result<List<LibraryReservation>> {
+        throw FakeGetDatabaseServiceFailedException()
+    }
+
+    override suspend fun getReservationList(userId: String): Result<List<List<String>>> {
+        throw FakeGetDatabaseServiceFailedException()
+    }
+
 }
 
 class FakeCheckDatabaseService:DatabaseService {
@@ -116,5 +249,69 @@ class FakeCheckDatabaseService:DatabaseService {
         onUpdate: (Int) -> Unit
     ): ListenerRegistration {
         return ListenerRegistration { }
+
+    override fun getLibraryStatus(
+        bookId: String,
+        callback: (LibraryHistory) -> Unit
+    ): ListenerRegistration {
+        throw FakeCheckDatabaseServiceFailedException()
     }
+
+    override suspend fun updateLibraryHistory(
+        userId: String,
+        libraryId: String,
+        bookId: String,
+        bookStatus: String,
+        bookTitle: String?,
+        bookAuthors: List<String>?,
+        keyword: String,
+        page: String
+    ): Result<Unit> {
+        throw FakeCheckDatabaseServiceFailedException()
+    }
+
+    override suspend fun getUserLoanBookList(userId: String): Result<List<UserLoanLibrary>> {
+        throw FakeCheckDatabaseServiceFailedException()
+    }
+
+    override suspend fun getUserLoanHistoryList(userId: String): Result<List<UserLoanLibrary>> {
+        throw FakeCheckDatabaseServiceFailedException()
+    }
+
+    override suspend fun getLoanDueStatus(
+        userId: String,
+        keyword: String,
+        page: String
+    ): Result<DueCheckResult> {
+        throw FakeCheckDatabaseServiceFailedException()
+    }
+
+    override suspend fun isOverdueBook(userId: String): Result<Boolean> {
+        throw FakeCheckDatabaseServiceFailedException()
+    }
+
+    override suspend fun isReservedBook(bookId: String): Result<Boolean> {
+        throw FakeCheckDatabaseServiceFailedException()
+    }
+
+    override suspend fun isUserReservedBook(userId: String): Result<Boolean> {
+        throw FakeCheckDatabaseServiceFailedException()
+    }
+
+    override suspend fun getLibraryReservationCount(bookId: String): Result<Int> {
+        throw FakeCheckDatabaseServiceFailedException()
+    }
+
+    override suspend fun getReservedStatus(userId: String, bookId: String): Result<String> {
+        throw FakeCheckDatabaseServiceFailedException()
+    }
+
+    override suspend fun checkMyReservationTurn(userId: String): Result<List<LibraryReservation>> {
+        throw FakeCheckDatabaseServiceFailedException()
+    }
+
+    override suspend fun getReservationList(userId: String): Result<List<List<String>>> {
+        throw FakeCheckDatabaseServiceFailedException()
+    }
+
 }
