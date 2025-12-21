16262a59fc55088e3763f947f9761e5d40548903
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-12-21 15:45:49 +0900

    Test: Create FakeDatabaseServiceExceptions

---
+++ Library/app/src/test/java/com/example/library/fake/service/FakeDatabaseServiceExceptions.kt
@@ -0,0 +1,5 @@
+package com.example.library.fake.service
+
+class FakeSaveDatabaseServiceFailedException:Exception("FakeSaveDatabaseService 테스트 실패")
+class FakeGetDatabaseServiceFailedException:Exception("FakeGetDatabaseService 테스트 실패")
+class FakeCheckDatabaseServiceFailedException:Exception("FakeCheckDatabaseService 테스트 실패")
