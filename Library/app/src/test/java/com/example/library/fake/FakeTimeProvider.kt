c2f7546d69dad704212d255ba1aca4799db36626
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-12-23 14:28:06 +0900

    Feat: Add before() and overdue()

--- Library/app/src/test/java/com/example/library/fake/FakeTimeProvider.kt
+++ Library/app/src/test/java/com/example/library/fake/FakeTimeProvider.kt
@@ -1,7 +1,23 @@
 package com.example.library.fake
 
 import com.example.library.core.TimeProvider
+import java.time.Instant
+import java.time.ZoneId
 
 class FakeTimeProvider:TimeProvider {
+
+    private val zoneId="Asia/Seoul"
+    private val seoulZone: ZoneId = ZoneId.of(zoneId)
+
     override fun now(): Long = 1_700_000_000_000L//2023년
+
+    fun before():Long{
+        val loanDateTime = Instant.ofEpochMilli(now()).atZone(seoulZone)
+        return loanDateTime.plusDays(-1).toInstant().toEpochMilli()
+    }
+
+    fun overdue():Long{
+        val loanDateTime = Instant.ofEpochMilli(now()).atZone(seoulZone)
+        return loanDateTime.plusDays(1).toInstant().toEpochMilli()
+    }
 }
