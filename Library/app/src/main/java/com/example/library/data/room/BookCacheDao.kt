7832895571da5c41f5b215d9d90b94e6ba303e63
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-12-26 20:02:19 +0900

    Feat: Add delete dao for expired cache

--- Library/app/src/main/java/com/example/library/data/room/BookCacheDao.kt
+++ Library/app/src/main/java/com/example/library/data/room/BookCacheDao.kt
@@ -43,6 +43,41 @@ interface BookCacheDao {
     @Query("UPDATE search_result SET accessedAt=:now WHERE libraryId = :libraryId AND page = :page")
     suspend fun updateLastAccess(libraryId:String, page:Int, now:Long)
 
+    @Query("SELECT * FROM search_result WHERE cachedAt < :cachedAt AND accessedAt < :accessedAt")
+    suspend fun getExpiredSearchResult(cachedAt: Long, accessedAt: Long):List<SearchResultWithLibrary>
+
+    @Query("DELETE FROM search_result WHERE bookId = :bookId")
+    suspend fun deleteSearchResult(bookId: String)
+
+    @Query("DELETE FROM library WHERE bookId = :bookId")
+    suspend fun deleteLibrary(bookId:String)
+
+    @Query("DELETE FROM book WHERE id = :bookId")
+    suspend fun deleteBook(bookId:String)
+
+    @Query("DELETE FROM book_image WHERE id = :bookId")
+    suspend fun deleteBookImage(bookId:String)
+
+    @Query("DELETE FROM search_total_count WHERE `query` = :query")
+    suspend fun deleteSearchTotalCount(query:String)
+
+    @Transaction
+    suspend fun deleteExpiredBooks(cachedAt: Long, accessedAt: Long){
+        val list= getExpiredSearchResult(cachedAt, accessedAt)
+        list.distinctBy { it.searchResultEntity.query }
+            .forEach {
+                deleteSearchTotalCount(it.searchResultEntity.query)
+            }
+
+        list.forEach {
+            val bookId=it.searchResultEntity.bookId
+            deleteLibrary(bookId)
+            deleteBook(bookId)
+            deleteBookImage(bookId)
+            deleteSearchResult(bookId)
+        }
+    }
+
     @Query("SELECT count FROM SEARCH_TOTAL_COUNT WHERE `query` = :keyword")
     suspend fun getSearchTotalCount(keyword: String): Int?
 
