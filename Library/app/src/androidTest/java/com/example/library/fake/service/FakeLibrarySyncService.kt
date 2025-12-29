a2f7ba10ff2b7d9fb4516692386572549329980e
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-12-29 22:37:52 +0900

    Refactor: Update method return type to non-null

--- Library/app/src/androidTest/java/com/example/library/fake/service/FakeLibrarySyncService.kt
+++ Library/app/src/androidTest/java/com/example/library/fake/service/FakeLibrarySyncService.kt
@@ -4,8 +4,8 @@ import com.example.library.data.entity.Library
 import com.example.library.domain.LibrarySyncService
 
 class FakeLibrarySyncService:LibrarySyncService {
-    override suspend fun getSearchBooks(keyword: String, pageNumber: Int): List<Library>? =
+    override suspend fun getSearchBooks(keyword: String, pageNumber: Int): List<Library> =
         emptyList()
 
-    override suspend fun getTotalCntForKeyword(keyword: String): Int? = 0
+    override suspend fun getTotalCntForKeyword(keyword: String): Int = 0
 }
