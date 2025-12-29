b88be04dac2a728c9641b05feb00a4b67e888607
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-12-30 03:26:51 +0900

    Refactor: Rename file

---
+++ Library/app/src/androidTest/java/com/example/library/fake/repository/FakeNetworkBookRepository.kt
@@ -0,0 +1,13 @@
+package com.example.library.fake.repository
+
+import com.example.library.data.entity.Item
+import com.example.library.domain.RemoteRepository
+import com.example.library.data.mapper.toItem
+import com.example.library.fake.FakeDataSource
+
+class FakeNetworkBookRepository : RemoteRepository {
+    override suspend fun searchVolume(query: String, limit: Int, offset: Int): Result<Item> {
+        val item= FakeDataSource.getListRange(limit, offset).toItem()
+        return Result.success(item)
+    }
+}
